6724db0527303ba5773d285854b28bc17409a281
Jaimewww <jaimelandazuri719@gmail.com>
2025-07-06 12:07:47 -0500

    Entity user using Hibernate

---
+++ src/main/java/edu/cc/notecloud/persistance/util/HibernateUtils.java
@@ -0,0 +1,21 @@
+package edu.cc.notecloud.persistance.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Error al crear SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
