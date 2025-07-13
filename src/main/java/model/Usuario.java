921d1a6f2f930e9117295f8021890aa12b35f68a
wilsonpalma <wilsonpalmasamaniego@gmail.com>
2025-07-13 13:19:40 -0500

    login de google terminado

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Usuario {
+    private Long id;
+    private String googleId;
+    private String nombre;
+    private String email;
+    private String imagenUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGoogleId() {
+        return googleId;
+    }
+
+    public void setGoogleId(String googleId) {
+        this.googleId = googleId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+}
